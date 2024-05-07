e1c936cf8a77298adc6a6df73b460cee8c9baade
elaine <sylviayangyijing@gmail.com>
2024-05-07 14:48:38 -0500

    add logging

--- src/main/java/com/elaine/customer/CustomerService.java
+++ src/main/java/com/elaine/customer/CustomerService.java
@@ -1,12 +1,16 @@
 package com.elaine.customer;
 
 import com.elaine.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CustomerService {
+
+    private final static Logger LOGGER= LoggerFactory.getLogger(CustomerService.class);
     private final CustomerRepository customerRepository;
 
     public CustomerService(CustomerRepository customerRepository) {
@@ -18,11 +22,16 @@ public class CustomerService {
 //    }
 
     List<Customer> getCustomers(){
+        LOGGER.info("getCustomers was called");
         return customerRepository.findAll();
     }
 
     Customer getCustomer(Long id){
         return customerRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException("customer with id " + id + " not found"));
+                .orElseThrow(() -> {
+                    NotFoundException notFoundException = new NotFoundException("customer with id " + id + " not found");
+                    LOGGER.error("error in getting Customer {}", id, notFoundException);
+                    return notFoundException;
+                });
     }
 }
